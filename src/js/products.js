8afa452603d455e657b8119a8eac842022f98ff0
Pablo <pablovicente19@hotmail.com>
2024-02-23 12:30:10 -0300

    firt commtit

---
+++ src/js/products.js
@@ -0,0 +1,58 @@
+const pizzas = [
+  {
+    id: 1,
+    name: "La Mr.Pit",
+    description: "Solo para expertos.",
+    image: "assets/images/pizzas/la-pit.jpg",
+    price: 350,
+  },
+  {
+    id: 2,
+    name: "Q' Jamone",
+    description: "Jamón Crudo.",
+    image: "assets/images/pizzas/la-Q'jamone.jpg",
+    price: 350,
+  },
+  {
+    id: 3,
+    name: "La Charly Garcia",
+    description: "Basta.",
+    image: "assets/images/pizzas/la-charly-garcia.jpg",
+    price: 380,
+  },
+  {
+    id: 4,
+    name: "Picantovich",
+    description: "Pica 2 Veces.",
+    image: "assets/images/pizzas/la-picantovich.jpg",
+    price: 750,
+  },
+  {
+    id: 5,
+    name: "La Hasbulla",
+    description: "En honor al 1.",
+    image: "assets/images/pizzas/la-hasbulla.jpg",
+    price: 990,
+  },
+  {
+    id: 6,
+    name: "La Leo Messi",
+    description: "¡De pie Señores!",
+    image: "assets/images/pizzas/la-messi.jpg",
+    price: 10,
+  },
+  {
+    id: 7,
+    name: "Nick Gi",
+    description: "La que desaparece.",
+    image: "assets/images/pizzas/la-nickgi.jpg",
+    price: 790,
+  },
+  {
+    id: 8,
+    name: "La Mr.Pit",
+    description: "Solo para expertos",
+    image: "assets/images/pizzas/la-pit.jpg",
+    price: 350,
+  },
+];
